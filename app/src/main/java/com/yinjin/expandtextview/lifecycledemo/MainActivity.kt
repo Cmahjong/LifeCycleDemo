1805dd1cdad068285c4d2e66dc4d5da2fbf03d7c
yj <1073279000@qq.com>
2018-05-25 14:01:17 +0800

    viewModel+LiveData 实现数据持久化

--- app/src/main/java/com/yinjin/expandtextview/lifecycledemo/MainActivity.kt
+++ app/src/main/java/com/yinjin/expandtextview/lifecycledemo/MainActivity.kt
@@ -1,18 +1,51 @@
 package com.yinjin.expandtextview.lifecycledemo
 
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.yinjin.expandtextview.lifecycledemo.observer.MyObserver
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     val myObserver: MyObserver by lazy {
         MyObserver(this)
     }
 
+    lateinit var viewModel: MyViewMoudle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MyViewMoudle::class.java)
         setContentView(R.layout.activity_main)
         lifecycle.addObserver(myObserver)
+        observer()
+        setValue()
     }
 
+    private fun setValue() {
+        doAsync {
+            for (i in 0 until 20) {
+                Thread.sleep(1000)
+                runOnUiThread {
+                    viewModel.name.value = (i + 20).toString().takeIf { it != "28" } ?: "這個是28的數據"
+                }
+            }
+        }
+
+    }
+
+    private fun observer() {
+        viewModel.name.observe(this, Observer<String> {
+            tv1.text = it ?: ""
+        })
+    }
+
+}
+
+class MyViewMoudle : ViewModel() {
+    var name = MutableLiveData<String>()
 }
